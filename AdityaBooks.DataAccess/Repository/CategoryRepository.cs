f329b74bae63d6031631c5919479b737c421e1ea
W0835715 <W0835715@ADL-A1201K-07>
2023-10-31 18:01:42 -0400

    2.2.5 Repository With README.txt

---
+++ AdityaBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using AdityaBooks.DataAccess.Repository.IRepository;
+using AdityaBooks.Models;
+using AdityaBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdityaBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                //_db.SaveChanges();
+            }
+        }
+    }
+}
